28b0e33d8ac04471abf5b379c760cc1dd6cbb132
zenonalex <zenon_er@hotmail.com>
2024-07-06 14:01:37 -0300

    feat: created usecases

---
+++ lib/features/movie_list/domain/usecases/get_top_rated_movies_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:appflix/core/errors/failures.dart';
+import 'package:appflix/features/movie_list/domain/entities/movie_list.dart';
+import 'package:appflix/features/movie_list/domain/repositories/i_movie_list_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetTopRatedMoviesUsecase {
+  final IMovieListRepository repository;
+
+  GetTopRatedMoviesUsecase(this.repository);
+
+  Future<Either<Failure, MovieList>> call() async {
+    return await repository.getTopRatedMovies();
+  }
+}
