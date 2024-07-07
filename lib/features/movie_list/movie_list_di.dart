662ac584b0ba3c12348baa0e40e6da4a98bbcef1
zenonalex <zenon_er@hotmail.com>
2024-07-07 10:45:58 -0300

    feat: added datasource dependency injection

--- lib/features/movie_list/movie_list_di.dart
+++ lib/features/movie_list/movie_list_di.dart
@@ -1,4 +1,5 @@
 import '../../di.dart';
+import 'data/data_sources.dart/movie_list_data_source.dart';
 import 'data/repositories/movie_list_repository.dart';
 import 'domain/repositories/i_movie_list_repository.dart';
 import 'domain/usecases/get_movie_list_usecase.dart';
@@ -6,9 +7,10 @@ import 'presentation/bloc/movie_list_bloc.dart';
 
 Future<void> initMovieList() async {
   // Data
-  sl.registerLazySingleton<IMovieListRepository>(() => MovieListRepository(sl()));
+  sl.registerLazySingleton<IMovieListDataSource>(() => MovieListDataSource(sl()));
 
   // Domain
+  sl.registerLazySingleton<IMovieListRepository>(() => MovieListRepository(sl()));
   sl.registerLazySingleton(() => GetMovieListUsecase(sl()));
 
   // Presentation
