93af22f90f68cc67db8717de282ea4b0a2ede1d2
zenonalex <zenon_er@hotmail.com>
2024-07-07 09:18:55 -0300

    feat: added movieListBloc in di

--- lib/features/movie_list/movie_list_di.dart
+++ lib/features/movie_list/movie_list_di.dart
@@ -1,7 +1,11 @@
 import '../../di.dart';
 import 'domain/usecases/get_movie_list_usecase.dart';
+import 'presentation/bloc/movie_list_bloc.dart';
 
 Future<void> initMovieList() async {
   // Domain
   sl.registerLazySingleton(() => GetMovieListUsecase(sl()));
+
+  // Presentation
+  sl.registerLazySingleton(() => MovieListBloc(sl()));
 }
