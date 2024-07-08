9b0ba6a9113d346bebbc18c368aa143dcc65e579
zenonalex <zenon_er@hotmail.com>
2024-07-08 19:01:22 -0300

    feat: di setup

--- lib/di.dart
+++ lib/di.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 
 import 'core/core_di.dart';
+import 'features/movie_details/movie_details_di.dart';
 import 'features/movie_list/movie_list_di.dart';
 
 final sl = GetIt.instance;
@@ -8,4 +9,5 @@ final sl = GetIt.instance;
 Future<void> init() async {
   initCore();
   initMovieList();
+  initMovieDetails();
 }
---
+++ lib/features/movie_details/movie_details_di.dart
@@ -0,0 +1,16 @@
+import '../../di.dart';
+import 'data/data_sources/movie_details_data_source.dart';
+import 'data/repositories/movie_details_repository.dart';
+import 'domain/repositories/i_movie_details_repository.dart';
+import 'domain/usecases/get_movie_details_usecase.dart';
+
+Future<void> initMovieDetails() async {
+  // Data
+  sl.registerLazySingleton<IMovieDetailsDataSource>(() => MovieDetailsDataSource(sl()));
+
+  // Domain
+  sl.registerLazySingleton<IMovieDetailsRepository>(() => MovieDetailsRepository(sl()));
+  sl.registerLazySingleton(() => GetMovieDetailsUsecase(sl()));
+
+  // Presentation
+}
